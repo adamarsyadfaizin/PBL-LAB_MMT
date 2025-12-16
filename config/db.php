892aeb70dbc84f75f1fe8caf3c5b49d6a2b4327b
Raihanbujeee <bujexixi@gmail.com>
2025-12-16 22:19:09 +0700

    mmm

--- config/db.php
+++ config/db.php
@@ -3,11 +3,7 @@ $host = 'localhost';
 $port = '5432';
 $dbname = 'db_pbl';
 $user = 'postgres';
-<<<<<<< HEAD
-$password = '1234567890'; 
-=======
-$password = '12345678'; 
->>>>>>> c562a0fff9231aa130afce51b703e0c5bded7908
+$password = 'raihan'; 
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
